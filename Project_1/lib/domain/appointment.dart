29563cb7a89a1aba5dca4e1a5fc4b9a1ab57610e
Srykimsour <soursour3001@gmail.com>
2025-11-03 15:26:54 +0700

    class

---
+++ Project_1/lib/domain/appointment.dart
@@ -0,0 +1,59 @@
+class Patient {
+  int id;
+  String name;
+  String gender;
+  int age;
+  String contact;
+
+  Patient({
+    required this.id,
+    required this.name,
+    required this.gender,
+    required this.age,
+    required this.contact,
+  });
+
+  void bookAppointment() {}
+
+  void viewAppointents() {}
+}
+
+class Appointment {
+  int id;
+  Date date;
+  Time time;
+  String reason;
+  String status;
+  List<Doctor> doctorID;
+  List<Patient> patientID;
+
+  Appointment({
+    required this.id,
+    required this.date,
+    required this.time,
+    required this.reason,
+    required this.status,
+    required this.doctorID,
+    required this.patientID,
+  });
+  void schedule() {}
+
+  void cancel() {}
+}
+
+class Doctor {
+  int id;
+  String name;
+  String specialization;
+  String contact;
+
+  Doctor({
+    required this.id,
+    required this.name,
+    required this.specialization,
+    required this.contact,
+  });
+  void viewAppointments() {}
+
+  void createMeeting() {}
+}
